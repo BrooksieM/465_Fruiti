efed03fcede8cc8f8f9b20a4fbb6ac482692f078
TilNexHorizon <johnson306111@gmail.com>
2025-09-25 11:23:11 -0400

    Code merging
    merging the json array and the get logic

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const app = express();
 app.use(express.json());
 
 // the array for recipes
-let recipes = [];
+let recipes = [
+    {id: 1, name: 'Apple Pie', ingredients: ['apple']},
+    {id: 2, name: 'Blueberry Pie', ingredients: ['blueberry']}
+];
 
 // logic for posting recipe
 app.post('/api/recipes', (req, res) => {
@@ -16,13 +19,12 @@ app.post('/api/recipes', (req, res) => {
     res.status(201).json(newRecipe);
 });
 
+//GET endpoint to retrive all products 
 app.get('/api/recipes', (req, res) => {
+        //Return the array of recipe
     res.json(recipes);
 });
 
-app.get('/', (req, res) => {
-    res.send('this is the post recipe part');
-});
 
 // this is starting the server
 const PORT = 3000;
