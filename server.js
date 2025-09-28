7aeb6e9bec1d7bd43974c56b9e7f9378a6d4fe1f
TilNexHorizon <johnson306111@gmail.com>
2025-09-28 15:58:24 -0400

    Code tested and Commented

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
+//server.js
+
 const express = require('express');
 const app = express();
 app.use(express.json());
 
 console.log('Loaded file:', __filename);
-// the array for recipes
 
+// the array for recipes
 let recipes = [
-    {id: 1, name: 'Apple Pie', ingredients: ['apple']},
-    {id: 2, name: 'Blueberry Pie', ingredients: ['blueberry']}
+    {id: 1, name: 'Apple Pie', ingredients: ['apple'], instructions: ['Make an apple pie']},
+    {id: 2, name: 'Blueberry Pie', ingredients: ['blueberry'], instructions: ['Make an blueberry']}
 ];
 
-// logic for posting recipe
+// POST endpoint to create a product
 app.post('/api/recipes', (req, res) => {
     const { name, ingredients, instructions } = req.body;
+    //Check if all required fields are satisfied
     if (!name || !ingredients || !instructions) {
         return res.status(400).json({ error: 'Missing required fields' });
     }
+    //Auto increments the id number
     const newRecipe = { id: recipes.length + 1, name, ingredients, instructions };
     recipes.push(newRecipe);
+    //Respond with a 201 status code with the new Recipe
     res.status(201).json(newRecipe);
 });
 
 //GET endpoint to retrive all products 
 app.get('/api/recipes', (req, res) => {
-        //Return the array of recipe
+    //Return the array of recipe in JSON format
     res.json(recipes);
 });
 
@@ -31,15 +36,19 @@ app.get('/', (req, res) => {
     res.send('this is the post recipe part');
 });
 
-app.delete('/api/recipe/:id', (req, res) => {
+//DELETE endpoint to delete a product with the specified id
+app.delete('/api/recipes/:id', (req, res) => {
   const id = Number(req.params.id);
+  //Validating the input
   if (!Number.isInteger(id) || id <= 0) {
     return res.status(400).json({ error: 'Invalid recipe ID' });
   }
+  //Check if there's an recipe with the ID specified by the user
   const idx = recipes.findIndex(r => r.id === id);
   if (idx === -1) {
     return res.status(404).json({ error: 'Recipe not found' });
   }
+  //Delete recipe
   const [deletedRecipe] = recipes.splice(idx, 1);
   res.status(200).json({ message: 'Recipe deleted', deletedRecipe });
 });
@@ -48,5 +57,5 @@ app.delete('/api/recipe/:id', (req, res) => {
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-    console.log(`Routes:\n  GET    /\n  GET    /api/recipe\n  DELETE /api/recipe/:id`);
+    console.log(`Routes:\n  POST   /api/recipes    \n  GET    /api/recipes\n  DELETE /api/recipes/:id`);
 });
