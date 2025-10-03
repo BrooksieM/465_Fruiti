0ff2c9aae0832367f313e5a57c6a0aff27848682
Ryan Brooks <rmb6616@psu.edu>
2025-10-03 12:50:11 -0400

    edit & read service
    added edit & read services

--- server.js
+++ server.js
@@ -53,6 +53,44 @@ app.delete('/api/recipes/:id', (req, res) => {
   res.status(200).json({ message: 'Recipe deleted', deletedRecipe });
 });
 
+//edit a recipe
+app.put('/api/recipes/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const { name, ingredients, instructions } = req.body;
+    //Validating the input
+    if (!Number.isInteger(id) || id <= 0) {
+        return res.status(400).json({ error: 'Invalid recipe ID' });
+    }
+    if (!name || !ingredients || !instructions) {
+        return res.status(400).json({ error: 'Missing required fields' });
+    }
+    //Check if there's an recipe with the ID specified by the user
+    const recipe = recipes.find(r => r.id === id);
+    if (!recipe) {
+        return res.status(404).json({ error: 'Recipe not found in System' });
+    }
+    //editing a recipe
+    recipe.name = name;
+    recipe.ingredients = ingredients;
+    recipe.instructions = instructions;
+    res.status(200).json(recipe);
+});
+
+//system reads recipe
+app.get('/api/recipes/:id', (req, res) => {
+    const id = Number(req.params.id);
+    //Validating the input (copied from edit a recipe)
+    if (!Number.isInteger(id) || id <= 0) {
+        return res.status(400).json({ error: 'Invalid recipe ID' });
+    }
+    //check if recipe exists by user
+    const recipe = recipes.find(r => r.id === id);
+    if (!recipe) {
+        return res.status(404).json({ error: 'Recipe not found in System' });
+    }
+    res.status(200).json(recipe);
+});
+
 // this is starting the server
 const PORT = 3000;
 app.listen(PORT, () => {
@@ -60,20 +98,3 @@ app.listen(PORT, () => {
     console.log(`Routes:\n  POST   /api/recipes    \n  GET    /api/recipes\n  DELETE /api/recipes/:id`);
 });
 
-
-/*
-# API Testing Guide
-Follow the steps below to test the `POST`, `GET`, and `DELETE` methods after starting the server.
-
-POST method
-	Copy and paste the following command into CMD after starting the server:
-		curl -X POST http://localhost:3000/api/recipes -H "Content-Type: application/json" -d "{\"name\":\"Strawberry Pie\",\"ingredients\":[\"strawberries\",\"sugar\",\"flour\"],\"instructions\":[\"Mix ingredients\",\"Bake for 30 minutes\"]}"
-
-GET method
-	Open the following link in your browser:
-		http://localhost:3000/api/recipes
-
-DELETE method
-	Copy and paste the code below to CMD after the server is started
-		curl -X DELETE http://localhost:3000/api/recipes/1
-*/
