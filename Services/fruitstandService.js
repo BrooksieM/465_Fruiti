6e07a9defefa6e840c4b59bbaf750e368310c285
Ryan Brooks <rmb6616@psu.edu>
2025-10-19 14:15:37 -0400

    added first swagger doc for POST
    only in fruitstands

--- Services/fruitstandService.js
+++ Services/fruitstandService.js
@@ -2,32 +2,6 @@
 
 // GET endpoint to search for nearby fruit stands
 module.exports = (app, supabase) => {
-    /**
-   * @swagger
-   * /fruitstands/search:
-   *   get:
-   *     summary: Get all fruit stands
-   *     description: Returns a list of all fruit stands from the database
-   *     responses:
-   *       200:
-   *         description: A list of fruit stands
-   *         content:
-   *           application/json:
-   *             schema:
-   *               type: array
-   *               items:
-   *                 type: object
-   *                 properties:
-   *                   id:
-   *                     type: string
-   *                     description: Fruit stand ID
-   *                   name:
-   *                     type: string
-   *                     description: Name of the fruit stand
-   *                   location:
-   *                     type: string
-   *                     description: Location of the fruit stand
-   */
 app.get('/api/fruitstands/search', async (req, res) => {
     try 
     {
@@ -64,6 +38,58 @@ app.get('/api/fruitstands/search', async (req, res) => {
 });
 
 // POST endpoint to create a new fruit stand
+/**
+ * @swagger
+ * /api/fruitstands:
+ *   post:
+ *     summary: Create a new fruit stand
+ *     description: Adds a new fruit stand to the database
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - address
+ *               - city
+ *               - state
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: Name of the fruit stand
+ *               address:
+ *                 type: string
+ *                 description: Street address
+ *               city:
+ *                 type: string
+ *               state:
+ *                 type: string
+ *               zip:
+ *                 type: string
+ *               phone:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Fruit stand created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 fruitStand:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                     name:
+ *                       type: string
+ *                     address:
+ *                       type: string
+ */
 app.post('/api/fruitstands', async (req, res) => {
     try 
     {
