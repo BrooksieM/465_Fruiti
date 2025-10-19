5b791571161a331e710bcd3e2833c6705e08a2fa
Ryan Brooks <rmb6616@psu.edu>
2025-10-19 18:51:05 -0400

    updated server.js

--- server.js
+++ server.js
@@ -33,16 +33,16 @@ testConnection();
 console.log('Loaded file:', __filename);
 
 // SERVICES
-//require('./Services/accountService')(app, supabase);
-//require('./Services/adminService')(app, supabase);
-//require('./Services/articleService')(app, supabase);
-//require('./Services/authService')(app, supabase);
-//require('./Services/commentService')(app, supabase);
-//require('./Services/contactService')(app, supabase);
+require('./Services/accountService')(app, supabase);
+require('./Services/adminService')(app, supabase);
+require('./Services/articleService')(app, supabase);
+// require('./Services/authService')(app, supabase);
+require('./Services/commentService')(app, supabase);
+// require('./Services/contactService')(app, supabase);
 require('./Services/fruitstandService')(app, supabase);
-//require('./Services/nutritionService')(app, supabase);
-//require('./Services/recipeService')(app, supabase);
-//require('./Services/sellerApplicationService')(app, supabase);
+require('./Services/nutritionService')(app, supabase);
+require('./Services/recipeService')(app, supabase);
+require('./Services/sellerApplicationService')(app, supabase);
 
 //swagger 
 const swaggerSetup = require('./swagger/swagger');
